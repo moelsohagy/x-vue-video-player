b266b4d4294a5c90d4d4d3596525940b690a9f86
mohamed-elsohagy <mohamed.elsohagy@dafa.sa>
2023-01-31 16:26:31 +0200

    make the component is global

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VideoPlayer from './components/VideoPlayer.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VideoPlayer)
+app.mount('#app')
